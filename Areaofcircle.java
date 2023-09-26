1b146e54d2b8673455ebfc0b63e13534b583c974
Alok Srivastav <97796215+AlokSrivastav23@users.noreply.github.com>
2023-09-26 10:10:00 +0530

    Create Areaofcircle.java

---
+++ Areaofcircle.java
@@ -0,0 +1,12 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        double pie, r, res;
+        pie = 3.14;
+        r = 2;
+        res=pie*r*r;
+        System.out.println(res);
+    }
+}
